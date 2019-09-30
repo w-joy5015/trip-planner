875220230343800268b82bd7c9df8b9700ba0716
Joy Wang <w.joy5015@gmail.com>
2019-09-30 12:50:14 -0400

    map and pointer added

--- Trip-Planner/src/index.js
+++ Trip-Planner/src/index.js
@@ -1,1 +1,18 @@
 console.log("HELLO")
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1Ijoidy1qb3kiLCJhIjoiY2sxNm05cW1rMGVraDNhbzg0NmJ4c2k2ZiJ9.mD-V5it440Me4R28-sLEyA";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+
+var el = document.createElement('div');
+el.className = 'marker';
+
+new mapboxgl.Marker(el).setLngLat([-74.009151, 40.705086]).addTo(map);
